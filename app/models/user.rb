fef32817673f4a3c3be7361ce4d8a6ea8fc723e5
wataru0327 <watarurataw00@icloud.com>
2025-08-29 11:08:17 +0900

    devise導入後ユーザーモデルのバリデーション設定

---
+++ app/models/user.rb
@@ -0,0 +1,24 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+ 
+  validates :nickname, presence: true
+  
+
+  validates :last_name, :first_name, presence: true,
+            format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: "全角文字を使用してください" }
+
+
+  validates :last_name_kana, :first_name_kana, presence: true,
+            format: { with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナを使用してください" }
+
+
+  validates :birthday, presence: true
+
+ 
+  validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/,
+                                 message: "英字と数字の両方を含めて設定してください" }
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'items/index'
   # トップページを items#index に設定
   root "items#index"
