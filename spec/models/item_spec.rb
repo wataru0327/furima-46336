a56d1deb6cb1595870d6cec1774a9c04a0ce7374
wataru0327 <watarurataw00@icloud.com>
2025-09-01 14:55:20 +0900

    商品出品機能追加後コード修正３

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Item, type: :model do
   before do
-    @item = FactoryBot.build(:item, user: user)  
+    @item = FactoryBot.build(:item)  
   end
 
   describe '商品出品機能' do
