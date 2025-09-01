5ffd86b071470c639853ea3597bc3c8ab0dfe4c3
wataru0327 <watarurataw00@icloud.com>
2025-09-01 12:18:36 +0900

    itemモデル単体テストコード作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,13 +1,22 @@
 FactoryBot.define do
   factory :item do
-    name { "MyString" }
-    description { "MyText" }
-    category_id { 1 }
-    condition_id { 1 }
-    shipping_charge_id { 1 }
-    prefecture_id { 1 }
-    days_to_ship_id { 1 }
-    price { 1 }
-    user { nil }
+    name { "テスト商品" }
+    description { "これはテスト用の商品説明です" }
+    category_id { 2 }
+    condition_id { 2 }
+    shipping_charge_id { 2 }
+    prefecture_id { 2 }
+    days_to_ship_id { 2 }
+    price { 500 }
+
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(
+        io: File.open(Rails.root.join('spec/fixtures/files/test_image.png')),
+        filename: 'test_image.png',
+        content_type: 'image/png'
+      )
+    end
   end
 end
