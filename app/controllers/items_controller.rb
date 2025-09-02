6dbb1dc8978bd1de22e1f532c38a84c185bbc24b
wataru0327 <watarurataw00@icloud.com>
2025-09-02 17:31:02 +0900

    商品削除機能追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]  
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.includes(:user).order(created_at: :desc)
@@ -19,10 +21,32 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    update_params = item_params
+    update_params = update_params.except(:image) if update_params[:image].blank?
+
+    if @item.update(update_params)
+      redirect_to @item, notice: '商品情報を更新しました'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path, notice: '商品を削除しました'
   end
 
   private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
 
   def item_params
     params.require(:item).permit(
@@ -35,6 +59,10 @@ class ItemsController < ApplicationController
       :days_to_ship_id,
       :price, 
       :image
-      ).merge(user_id: current_user.id)
+    ).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    redirect_to root_path unless current_user.id == @item.user_id
   end
 end
