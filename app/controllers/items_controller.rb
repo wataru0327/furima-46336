fbda5fde98671d344eb9b604351c3ed422d29e89
wataru0327 <watarurataw00@icloud.com>
2025-09-02 15:44:29 +0900

    商品情報編集機能追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]  
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :move_to_index, only: [:edit, :update]
 
   def index
     @items = Item.includes(:user).order(created_at: :desc)
@@ -12,17 +14,35 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to root_path, notice: '商品を出品しました'
+      redirect_to root_path, notice: 
     else
       render :new, status: :unprocessable_entity
     end
   end
 
   def show
-    @item = Item.find(params[:id])
+  end
+
+  def edit
+    
+  end
+
+  def update
+    update_params = item_params
+    update_params = update_params.except(:image) if update_params[:image].blank?
+
+    if @item.update(update_params)
+      redirect_to @item, notice: 
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
 
   def item_params
     params.require(:item).permit(
@@ -35,6 +55,12 @@ class ItemsController < ApplicationController
       :days_to_ship_id,
       :price, 
       :image
-      ).merge(user_id: current_user.id)
+    ).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    if current_user.id != @item.user_id || @item.status == true
+      redirect_to root_path
+    end
   end
 end
