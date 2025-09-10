b63e3f36a40f0b00d19ebc9f3201d31ce012c441
wataru0327 <watarurataw00@icloud.com>
2025-09-10 18:56:01 +0900

    Update 20250828093220_devise_create_users.rb

--- db/migrate/20250828093220_devise_create_users.rb
+++ db/migrate/20250828093220_devise_create_users.rb
@@ -4,7 +4,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      t.string :email,              null: false, default: "", limit: 190
       t.string :encrypted_password, null: false, default: ""
 
       t.string :nickname,           null: false
@@ -13,7 +13,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.1]
       t.string :last_name_kana,     null: false
       t.string :first_name_kana,    null: false
       t.date   :birthday,           null: false
-  
 
       ## Recoverable
       t.string   :reset_password_token
@@ -22,13 +21,13 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.1]
       ## Rememberable
       t.datetime :remember_created_at
 
-
       t.timestamps null: false
     end
 
-    add_index :users, :email,                unique: true
+    add_index :users, :email, unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
 end
+
