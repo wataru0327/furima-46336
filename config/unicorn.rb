bd5851e861b0da95612e8cb5fa3e488ed4fbe753
wataru0327 <watarurataw00@icloud.com>
2025-09-12 12:40:15 +0900

    Fix unicorn working_directory path

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,57 +1,37 @@
-# サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
-app_path = File.expand_path('../../../current', __FILE__)
+# サーバ上でのアプリケーションコードが設置されているディレクトリ
+app_path = File.expand_path('../../../', __FILE__)
 
-# Rails環境を指定（productionをデフォルトにする）
+# Rails環境
 rails_env = ENV['RAILS_ENV'] || "production"
 
-# アプリケーションサーバの性能を決定する
+# Unicornの設定
 worker_processes 1
+working_directory "#{app_path}/current"
 
-# アプリケーションの設置されているディレクトリを指定
-working_directory app_path
+pid    "#{app_path}/shared/tmp/pids/unicorn.pid"
+listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
 
-# Unicornの起動に必要なファイルの設置場所を指定
-pid "#{app_path}/../shared/tmp/pids/unicorn.pid"
+stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
+stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
 
-# ポート番号を指定（ソケット通信）
-listen "#{app_path}/../shared/tmp/sockets/unicorn.sock"
-
-# エラーのログを記録するファイルを指定
-stderr_path "#{app_path}/../shared/log/unicorn.stderr.log"
-
-# 通常のログを記録するファイルを指定
-stdout_path "#{app_path}/../shared/log/unicorn.stdout.log"
-
-# Railsアプリケーションの応答を待つ上限時間を設定
 timeout 60
-
-# 以下は応用的な設定
 preload_app true
 GC.respond_to?(:copy_on_write_friendly=) && GC.copy_on_write_friendly = true
-
 check_client_connection false
 
-run_once = true
-
 before_fork do |server, worker|
-  defined?(ActiveRecord::Base) &&
-    ActiveRecord::Base.connection.disconnect!
-
-  if run_once
-    run_once = false # prevent from firing again
-  end
+  ActiveRecord::Base.connection.disconnect! if defined?(ActiveRecord::Base)
 
   old_pid = "#{server.config[:pid]}.oldbin"
   if File.exist?(old_pid) && server.pid != old_pid
     begin
       sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
       Process.kill(sig, File.read(old_pid).to_i)
-    rescue Errno::ENOENT, Errno::ESRCH => e
-      logger.error e
+    rescue Errno::ENOENT, Errno::ESRCH
     end
   end
 end
 
 after_fork do |_server, _worker|
-  defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
+  ActiveRecord::Base.establish_connection if defined?(ActiveRecord::Base)
 end
